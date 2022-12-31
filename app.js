d0f8364d9f88784b9c55c19a71a2faba00908b2c
Joon Ki Chun <joon0858@naver.com>
2022-12-31 15:59:26 +0900

    Add prettier rule, Modify routes directory

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
 const port = 8080;
-const usersRouter = require("./routes/users");
+
+const router = require("./routes");
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use("/api", [usersRouter]);
+app.use("/api", router);
 
 app.listen(port, () => {
   console.log(port, "포트로 서버가 켜졌어요!");
