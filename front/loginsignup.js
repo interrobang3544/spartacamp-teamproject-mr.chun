b5e07718fde966e90555e438216534b07fe0b3c9
iffy-yeti <lee129you@gmail.com>
2023-01-03 05:04:37 +0900

    로그인/가입 폼 error 알림창 빼고 완성

--- front/loginsignup.js
+++ front/loginsignup.js
@@ -26,10 +26,34 @@ const container = document.querySelector(".container"),
             })
         })
 
-// // 회원가입, 로그인 폼 나오게 하기
+// // 회원가입, 로그인 폼 클릭하면 나오게 하기
         $(document).on('click', '#signupform', function(){
             container.classList.add("active");
         });
         $(document).on('click', '#loginform', function(){
             container.classList.remove("active");
         });
+
+
+// 사장/고객 선택 버튼 작동
+var selectField = document.getElementById("selectField");
+var selectText = document.getElementById("selectText");
+var options = document.getElementsByClassName("options");
+var list = document.getElementById("list");
+var arrowIcon = document.getElementById("arrowIcon");
+
+// 선택하면 선택지 숨겨지게 하기
+selectField.onclick = function(){
+    list.classList.toggle("hide");
+    // 클릭하면 화살표 방향 바뀌게 하기
+    arrowIcon.classList.toggle("rotate");
+}
+
+for(option of options){
+    option.onclick = function(){
+        selectText.innerHTML = this.textContent;
+        list.classList.toggle("hide");
+        // 클릭하면 화살표 방향 바뀌게 하기
+        arrowIcon.classList.toggle("rotate");
+    }
+}
